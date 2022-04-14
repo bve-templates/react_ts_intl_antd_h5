f10dfb103551a930c6c64f6e93ac1ff3c3be0676
施华军 <shihj@yuntron.org>
2022-04-14 15:35:26 +0800

    add

---
+++ template/src/pages/home/index.tsx
@@ -0,0 +1,36 @@
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
+import Item from './components/item';
+import Loading from '@/components/loading';
+import styles from './index.scss';
+import { Button, Toast } from 'antd-mobile';
+
+function Home(props) {
+  const { list, loading, getList } = props;
+
+  useEffect(() => {
+    getList();
+  }, []);
+
+  return (
+    <div className={styles.home}>
+      <Button onClick={() => Toast.show('toast')}>Toast</Button>
+      {loading ? (
+        <Loading text="数据加载中" />
+      ) : (
+        list.map(item => <Item item={item} key={item.id} />)
+      )}
+    </div>
+  );
+}
+export default connect(
+  ({ loading: { effects }, home: { list } }) => ({
+    loading: effects['home/getList'],
+    list,
+  }),
+  (dispatch: any) => ({
+    getList() {
+      dispatch({ type: 'home/getList' });
+    },
+  }),
+)(React.memo(Home));
